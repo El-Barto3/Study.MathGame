dca55679271723e969b6796ce2ab74787ce07e2f
Barto <bartkosz111@gmail.com>
2025-02-03 19:52:29 +0100

    Made main menu with current functionality of basic and random game

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,22 +1,27 @@
 ﻿
 
-int selectedOption = 0;
+using System.Net.Sockets;
+
 int maxValueOfNumber = 100;
 string[] optionsNames = ["ADDITION", "SUBSTRACTION", "MULTIPLICATION", "DIVISION"];
+string[] mainMenuStrings = ["STANDARD MODE", "RANDOMIZER", "CHOOSE DIFFICULTY", "GAMES HISTORY"];
 char[] opperands = ['+', '-', '*', '/'];
+var randomVar = new Random();
 
+void handleReadKey()
+{
 
-void startBasicGame(int selectedOperationOption)
+}
+void startBasicGame(int selectedGame)
 {
     Console.Clear();
     Console.WriteLine("Calculate this equation:");
 
-    var randomVar = new Random();
     int correctAnswer = 0;
     int firstNumber = randomVar.Next(maxValueOfNumber);
     int secondNumber = randomVar.Next(maxValueOfNumber);
 
-    switch (selectedOperationOption)
+    switch (selectedGame)
     {
         case 0:
             correctAnswer = firstNumber + secondNumber;
@@ -33,7 +38,7 @@ void startBasicGame(int selectedOperationOption)
 
     }
 
-    Console.Write($"{firstNumber} {opperands[selectedOperationOption]} {secondNumber} = ");
+    Console.Write($"{firstNumber} {opperands[selectedGame]} {secondNumber} = ");
     var userAnswer = Console.ReadLine();
 
     if (userAnswer != null && userAnswer.Equals(correctAnswer.ToString()))
@@ -44,31 +49,36 @@ void startBasicGame(int selectedOperationOption)
     {
         Console.WriteLine("Wrong answer!");
     }
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
 
 }
 
-void showMenuForArythmeticModes()
+int showMenu(string[] namesTable, string menuHeader)
 {
     ConsoleKey clickedKey;
+    int selectedOption = 0;
     do
     {
         Console.Clear();
-        Console.WriteLine("Choose your game (UP/DOWN ARROWS): ");
+        if(menuHeader != null)
+            Console.WriteLine(menuHeader);
 
-        for (int i = 0; i < optionsNames.Length; i++)
+        for (int i = 0; i < namesTable.Length; i++)
         {
             if (selectedOption == i)
                 Console.Write("--> ");
-            Console.WriteLine(optionsNames[i]);
+            Console.WriteLine(namesTable[i]);
         }
 
+        handleReadKey();
         clickedKey = Console.ReadKey().Key;
 
         //UP click
         if (clickedKey == ConsoleKey.UpArrow)
         {
             if (selectedOption == 0)
-                selectedOption = 3;
+                selectedOption = namesTable.Length - 1;
             else
                 selectedOption -= 1;
         }
@@ -76,24 +86,47 @@ void showMenuForArythmeticModes()
         //DOWN click
         if (clickedKey == ConsoleKey.DownArrow)
         {
-            if (selectedOption == 3)
+            if (selectedOption == (namesTable.Length-1))
                 selectedOption = 0;
             else
                 selectedOption += 1;
+        }       
+        
+        //ESC click
+        if (clickedKey == ConsoleKey.Escape)
+        {
+            return -1;
         }
 
     } while (clickedKey != ConsoleKey.Enter);
 
+    return selectedOption;
 }
 
 
 //menu for choosing mode of game
 void main()
 {
+    int outcome;
+    //making game in loop until user quits
+    do
+    {
+        //show main menu
+        outcome = showMenu(mainMenuStrings, "Navigate with arrows and use ESC to go back");
 
-    showMenuForArythmeticModes();
-    startBasicGame(selectedOption);
-
+        //standard mode
+        if(outcome == 0)
+        {
+            var mode = showMenu(optionsNames, "Choose your game (UP/DOWN ARROWS):");
+            if (mode != -1)
+                startBasicGame(mode);
+        }
+        //randomizer
+        if (outcome == 1)
+        {
+            startBasicGame(randomVar.Next() % 4);
+        }
+    } while (outcome != -1);
 }
 
 main();
