5b8078e3c8541a8d95214413110057abcc61f885
Barto <bartkosz111@gmail.com>
2025-02-03 09:17:20 +0100

    Implementing working game modes menu

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! xxx -- zz z z");
-Console.WriteLine("Hello, World! xxx -- zz z z");
+
+string x;
+void startGame()
+{
+
+    ConsoleKey clickedKey;
+    int selectedOption = 0;
+    string[] optionsNames = ["ADDITION", "SUBSTRACTION", "MULTIPLICATION", "DIVISION"];
+
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("Choose your game (UP/DOWN ARROWS): ");
+        
+        for(int i = 0;i<optionsNames.Length;i++)
+        {
+            if (selectedOption == i)
+                Console.Write("--> ");
+            Console.WriteLine(optionsNames[i]);
+        }
+
+        clickedKey = Console.ReadKey().Key;
+
+        //UP click
+        if (clickedKey == ConsoleKey.UpArrow)
+        {
+            if (selectedOption == 0)
+                selectedOption = 3;
+            else
+                selectedOption -= 1;
+        }
+
+        //DOWN click
+        if (clickedKey == ConsoleKey.DownArrow)
+        {
+            if (selectedOption == 3)
+                selectedOption = 0;
+            else
+                selectedOption += 1;
+        }
+
+    } while (clickedKey != ConsoleKey.Enter);
+
+}
+
+startGame();
