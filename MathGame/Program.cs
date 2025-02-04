7b14cb71e3ad3051d85b9f7d7bc7b0fe9de6c290
Barto <bartkosz111@gmail.com>
2025-02-04 12:33:28 +0100

    Revert "Game history works for Randomizer mode"
    This reverts commit cc497995cc2e06593c08da09c5e67517f204b717.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -2,29 +2,18 @@
 
 using System.Net.Sockets;
 
-int[] maxValueOfNumber = { 10, 30, 100 };
-int[] minValueOfNumber = { 1, 10, 30 };
-string[] standardGames = ["ADDITION", "SUBSTRACTION", "MULTIPLICATION", "DIVISION"];
+int[] maxValueOfNumber = {10,30,100};
+string[] optionsNames = ["ADDITION", "SUBSTRACTION", "MULTIPLICATION", "DIVISION"];
 string[] mainMenuStrings = ["STANDARD MODE", "RANDOMIZER", "CHOOSE DIFFICULTY", "GAMES HISTORY"];
 string[] difficultiesLevel = ["EASY", "NORMAL", "HARD"];
 char[] opperands = ['+', '-', '*', '/'];
 var randomVar = new Random();
 int gameLength = 3;
 int difficultyLevel = 0;
-string[] gamesHistory = new string[10];
 
-int findLastNonEmpty(string[] array)
+void handleReadKey()
 {
-    int lastIndex = -1;
-    for (int i = array.Length - 1; i >= 0; i--)
-    {
-        if (array[i] != null)
-        {
-            lastIndex = i;
-            break; // Stop at the first non-empty element
-        }
-    }
-    return lastIndex;
+
 }
 
 int startBasicGame(int selectedGame, int currentRound, int maxRounds)
@@ -34,8 +23,8 @@ int startBasicGame(int selectedGame, int currentRound, int maxRounds)
 
     int correctAnswer = 0;
     //need to fix making numbers
-    int firstNumber = randomVar.Next(minValueOfNumber[difficultyLevel], maxValueOfNumber[difficultyLevel]);
-    int secondNumber = randomVar.Next(minValueOfNumber[difficultyLevel], maxValueOfNumber[difficultyLevel]);
+    int firstNumber = randomVar.Next(maxValueOfNumber[difficultyLevel]);
+    int secondNumber = randomVar.Next(maxValueOfNumber[difficultyLevel]);
 
     switch (selectedGame)
     {
@@ -56,20 +45,23 @@ int startBasicGame(int selectedGame, int currentRound, int maxRounds)
 
     Console.Write($"{firstNumber} {opperands[selectedGame]} {secondNumber} = ");
     var userAnswer = Console.ReadLine();
-    var point = 0;
+    var points = 0;
 
     if (userAnswer != null && userAnswer.Equals(correctAnswer.ToString()))
     {
         Console.WriteLine("Good job!");
-        point = 1;
+        points = 1;
     }
     else
+    {
         Console.WriteLine("Wrong answer!");
+        points = 0;
+    }
 
     Console.WriteLine("Press any key to " + (currentRound == maxRounds ? "exit" : "continue"));
     Console.ReadKey();
 
-    return point;
+    return points;
 
 }
 
@@ -89,12 +81,13 @@ int showMenu(string[] namesTable, string menuHeader, int selectedOption = 0)
             Console.WriteLine(namesTable[i]);
         }
 
+        handleReadKey();
         clickedKey = Console.ReadKey().Key;
 
         //UP click
         if (clickedKey == ConsoleKey.UpArrow)
         {
-            if (selectedOption <= 0)
+            if (selectedOption == 0)
                 selectedOption = namesTable.Length - 1;
             else
                 selectedOption -= 1;
@@ -103,7 +96,7 @@ int showMenu(string[] namesTable, string menuHeader, int selectedOption = 0)
         //DOWN click
         if (clickedKey == ConsoleKey.DownArrow)
         {
-            if (selectedOption >= (namesTable.Length-1))
+            if (selectedOption == (namesTable.Length-1))
                 selectedOption = 0;
             else
                 selectedOption += 1;
@@ -134,54 +127,23 @@ void main()
         //standard mode
         if(outcome == 0)
         {
-            var mode = showMenu(standardGames, "Choose your game (UP/DOWN ARROWS):");
+            var mode = showMenu(optionsNames, "Choose your game (UP/DOWN ARROWS):");
             if (mode == -1)
-                continue;
+                break;
             
             for (int i = 0; i < gameLength; i++)
                 startBasicGame(mode, (i + 1), gameLength);
-        }        
-        
-        //randomizer mode
-        if (outcome == 0)
-        {
-            var mode = showMenu(standardGames, "Choose your game (UP/DOWN ARROWS):");
-            if (mode == -1)
-                continue;
-
-            for (int i = 0; i < gameLength; i++)
-                startBasicGame(mode, (i + 1), gameLength);
         }
-
-        //randomizer and standard
-        if (outcome == 1 || outcome == 0)
+        //randomizer
+        if (outcome == 1)
         {
-            var pointsSum = 0;
             for(int i = 0;i<gameLength;i++)
-                pointsSum += startBasicGame((randomVar.Next() % standardGames.Length), (i + 1), gameLength);
-
-            var indexOfEmpty = Array.IndexOf(gamesHistory, null);
-            if (indexOfEmpty == -1)
-            {
-                indexOfEmpty = gamesHistory.Length;
-                Array.Resize(ref gamesHistory, (gamesHistory.Length + 10));
-            }
-
-            gamesHistory[indexOfEmpty] = $"No. {indexOfEmpty+1}\tDifficulty: {difficultiesLevel[difficultyLevel]}\tMode: {mainMenuStrings[1]}\tScore: {pointsSum} / {gameLength}";
+                startBasicGame((randomVar.Next() % optionsNames.Length), (i + 1), gameLength);
         }
-
-        //difficulty level
+        //randomizer
         if (outcome == 2)
         {
-            var newDifficulty = showMenu(difficultiesLevel, "Choose difficulty", difficultyLevel);
-            difficultyLevel = newDifficulty != -1 ? newDifficulty : difficultyLevel;
-        }   
-        
-        //games history panel
-        if (outcome == 3)
-        {
-            var lastValid = findLastNonEmpty(gamesHistory);
-            showMenu(gamesHistory[0..(lastValid + 1)], "Games history", -1); //dont show choosing arrow and null elements when loaded
+            difficultyLevel = showMenu(difficultiesLevel, "Choose difficulty", difficultyLevel);
         }
     } while (outcome != -1);
 }
